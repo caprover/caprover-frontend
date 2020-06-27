9a871fc45de9dea9704bcf898e06d22c402a0ef0
Kasra Bigdeli <kasra85@gmail.com>
2020-06-27 19:23:43 -0400

    Fixed broken legacy component

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,15 +1,11 @@
-import { Form } from '@ant-design/compatible'
-import '@ant-design/compatible/assets/index.css'
 import { LockOutlined } from '@ant-design/icons'
-import { Button, Card, Collapse, Input, Radio } from 'antd'
-import RadioGroup from 'antd/lib/radio/group'
+import { Button, Card, Collapse, Input, Radio, Row } from 'antd'
 import React from 'react'
 import { Redirect, RouteComponentProps } from 'react-router'
 import ApiManager from '../api/ApiManager'
 import StorageHelper from '../utils/StorageHelper'
 import Toaster from '../utils/Toaster'
 import ApiComponent from './global/ApiComponent'
-const FormItem = Form.Item
 
 const NO_SESSION = 1
 const SESSION_STORAGE = 2
@@ -58,7 +54,7 @@ export default class Login extends ApiComponent<RouteComponentProps<any>, any> {
                     }}
                 >
                     <Card title="CapRover Login" style={{ width: 350 }}>
-                        <WrappedNormalLoginForm
+                        <NormalLoginForm
                             onLoginRequested={(
                                 password: string,
                                 loginOption: number
@@ -80,92 +76,81 @@ const radioStyle = {
     lineHeight: '30px',
 }
 
-class NormalLoginForm extends React.Component<any, any> {
+class NormalLoginForm extends React.Component<
+    any,
+    {
+        loginOption: number
+        passwordEntered: string
+    }
+> {
     constructor(props: any) {
         super(props)
         this.state = {
             loginOption: NO_SESSION,
+            passwordEntered: ``,
         }
     }
 
-    handleSubmit = (e: any) => {
+    handleSubmit = () => {
         const self = this
-        e.preventDefault()
-        this.props.form.validateFields((err: any, values: any) => {
-            if (!err) {
-                self.props.onLoginRequested(
-                    values.password,
-                    self.state.loginOption
-                )
-            }
-        })
+        self.props.onLoginRequested(
+            self.state.passwordEntered,
+            self.state.loginOption
+        )
     }
 
     render() {
         const self = this
-        const { getFieldDecorator } = this.props.form
         return (
-            <Form onSubmit={this.handleSubmit} className="login-form">
-                <FormItem>
-                    {getFieldDecorator('password', {
-                        rules: [
-                            {
-                                required: true,
-                                message: 'Please input your Password!',
-                            },
-                        ],
-                    })(
-                        <Input.Password
-                            prefix={
-                                <LockOutlined
-                                    style={{ color: 'rgba(0,0,0,.25)' }}
-                                />
-                            }
-                            placeholder="Password"
-                            autoFocus
-                        />
-                    )}
-                </FormItem>
-                <FormItem>
-                    <Button
-                        style={{ float: 'right' }}
-                        type="primary"
-                        htmlType="submit"
-                        className="login-form-button"
-                    >
-                        Login
-                    </Button>
-                </FormItem>
-                <FormItem>
-                    <Collapse>
-                        <Collapse.Panel header="Remember Me" key="1">
-                            <RadioGroup
-                                onChange={(e) => {
-                                    self.setState({
-                                        loginOption: e.target.value,
-                                    })
-                                }}
-                                value={this.state.loginOption}
-                            >
-                                <Radio style={radioStyle} value={NO_SESSION}>
-                                    No session persistence (Most Secure)
-                                </Radio>
-                                <Radio
-                                    style={radioStyle}
-                                    value={SESSION_STORAGE}
-                                >
-                                    Use sessionStorage
-                                </Radio>
-                                <Radio style={radioStyle} value={LOCAL_STORAGE}>
-                                    Use localStorage (Most Persistent)
-                                </Radio>
-                            </RadioGroup>
-                        </Collapse.Panel>
-                    </Collapse>
-                </FormItem>
-            </Form>
+            <div>
+                <Input.Password
+                    prefix={
+                        <LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />
+                    }
+                    onChange={(e) => {
+                        self.setState({ passwordEntered: `${e.target.value}` })
+                    }}
+                    placeholder="Password"
+                    autoFocus
+                />
+                <div style={{ marginTop: 20, marginBottom: 20 }}>
+                    <Row justify="end">
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            className="login-form-button"
+                            onClick={() => {
+                                self.handleSubmit()
+                            }}
+                        >
+                            Login
+                        </Button>
+                    </Row>
+                </div>
+                <Collapse>
+                    <Collapse.Panel header="Remember Me" key="1">
+                        <Radio.Group
+                            onChange={(e) => {
+                                console.log(e.target.value)
+                                self.setState({
+                                    loginOption: e.target.value,
+                                })
+                            }}
+                            value={self.state.loginOption}
+                        >
+                            <Radio style={radioStyle} value={NO_SESSION}>
+                                No session persistence (Most Secure)
+                            </Radio>
+                            <Radio style={radioStyle} value={SESSION_STORAGE}>
+                                Use sessionStorage
+                            </Radio>
+                            <Radio style={radioStyle} value={LOCAL_STORAGE}>
+                                Use localStorage (Most Persistent)
+                            </Radio>
+                        </Radio.Group>
+                    </Collapse.Panel>
+                </Collapse>
+            </div>
         )
     }
 }
-
-const WrappedNormalLoginForm = Form.create<any>()(NormalLoginForm)
