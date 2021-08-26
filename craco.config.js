ec3346a320215983b3d0234b9f2e6d2cb40b9d5b
Kai Krause <kkrause95@gmail.com>
2021-08-26 16:53:48 +0200

    fixed autoprefixer

--- craco.config.js
+++ craco.config.js
@@ -3,21 +3,5 @@ module.exports = {
     webpack: {
         plugins: [],
     },
-    plugins: [
-        /*    {
-            plugin: CracoAntDPlugin,
-            options: {
-                lessLoaderOptions: {
-                    lessOptions: {
-                        javascriptEnabled: true,
-                    },
-                },
-                babelPluginImportOptions: {
-                    libraryName: 'antd',
-                    libraryDirectory: 'es',
-                    style: true,
-                },
-            },
-        },*/
-    ],
+    plugins: [],
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,25 @@
 const gulp = require('gulp')
 const gulpless = require('gulp-less')
-const postcss = require('gulp-postcss')
 const debug = require('gulp-debug')
 var csso = require('gulp-csso')
 const autoprefixer = require('autoprefixer')
 const NpmImportPlugin = require('less-plugin-npm-import')
 
 gulp.task('less', function () {
-    const plugins = [autoprefixer()]
-
-    return (
-        gulp
-            .src('src/styles/*-theme.less')
-            .pipe(debug({ title: 'Less files:' }))
-            .pipe(
-                gulpless({
-                    javascriptEnabled: true,
-                    plugins: [new NpmImportPlugin({ prefix: '~' })],
-                })
-            )
-            // .pipe(postcss(plugins))
-            .pipe(
-                csso({
-                    debug: true,
-                })
-            )
-            .pipe(gulp.dest('./public'))
-    )
+    return gulp
+        .src('src/styles/*-theme.less')
+        .pipe(debug({ title: 'Less files:' }))
+        .pipe(
+            gulpless({
+                javascriptEnabled: true,
+                plugins: [new NpmImportPlugin({ prefix: '~' })],
+            })
+        )
+        .pipe(autoprefixer())
+        .pipe(
+            csso({
+                debug: true,
+            })
+        )
+        .pipe(gulp.dest('./public'))
 })
