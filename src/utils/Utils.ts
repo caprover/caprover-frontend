f4269492b50d88a02b517623f4939f0383101f4b
Slava Vishnyakov <bomboze@gmail.com>
2024-03-20 11:22:34 +0300

    Also generate base64 and alnum random string

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -77,25 +77,53 @@ export default {
         return result
     },
 
-    replaceAllGenRandomForOneClickApp(inputString: string) {
-        let matches
+    createRandomStringAlnum(length: number) {
+        let result = ''
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        const charactersLength = characters.length
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength))
+        }
+        return result
+    },
 
-        while (
-            (matches = /\$\$cap_gen_random_hex\((\d+)\)/g.exec(inputString)) &&
-            matches.length === 2
-        ) {
-            const hexLength = Number(matches[1])
-            if (hexLength > 256) {
-                // capping out the maximum length to avoid unintentional problems
-                inputString = inputString.replace(matches[0], '')
-            } else {
-                inputString = inputString.replace(
-                    matches[0],
-                    `${this.createRandomStringHex(hexLength)}`
-                )
+    createRandomStringBase64(byteLength: number) {
+        let result = ''
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+        const bytes = new Uint8Array(byteLength)
+        window.crypto.getRandomValues(bytes)
+        for (let i = 0; i < byteLength; i++) {
+            result += characters[bytes[i] % 64]
+        }
+        if (byteLength % 3 !== 0) {
+            result += '='.repeat(3 - byteLength % 3)
+        }
+        return result
+    },
+    
+    replaceAllGenRandomForOneClickApp(inputString: string) {
+        const replacer = (match: string, p1: string, p2: string) => {
+            const length = Number(p2)
+            if (length > 256) {
+                return ''
+            }
+            switch (p1) {
+                case 'hex':
+                    return this.createRandomStringHex(length)
+                case 'base64':
+                    return this.createRandomStringBase64(length)
+                case 'alnum':
+                    return this.createRandomStringAlnum(length)
+                default:
+                    return match
             }
         }
-
+    
+        inputString = inputString.replace(
+            /\$\$cap_gen_random_(hex|base64|alnum|alnum_mixed)\((\d+)\)/g,
+            replacer
+        )
+    
         return inputString
     },
 
