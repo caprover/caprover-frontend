105537cd94b9330d6df0ceb01515c872f263370e
Kasra Bigdeli <kasra85@gmail.com>
2024-10-06 19:21:51 -0700

    Added French

--- src/utils/Language.ts
+++ src/utils/Language.ts
@@ -2,12 +2,14 @@ import { Locale } from 'antd/es/locale'
 import deDE from 'antd/es/locale/de_DE'
 import enUS from 'antd/es/locale/en_US'
 import esES from 'antd/es/locale/es_ES'
+import frFR from 'antd/es/locale/fr_FR'
 import idID from 'antd/es/locale/id_ID'
 import ptBR from 'antd/es/locale/pt_BR'
 import zhCN from 'antd/es/locale/zh_CN'
 import deDEMessages from '../locales/de-DE.json'
 import enUSMessages from '../locales/en-US.json'
 import esESMessages from '../locales/es-ES.json'
+import frFRMessages from '../locales/fr-FR.json'
 import idIDMessages from '../locales/id-ID.json'
 import ptBRMessages from '../locales/pt-BR.json'
 import zhCNMessages from '../locales/zh-CN.json'
@@ -65,6 +67,13 @@ const languagesOptions: LanguageOption[] = [
         alias: ['id'],
         antdLocale: idID,
         messages: idIDMessages,
+    },
+    {
+        label: 'French',
+        value: 'fr-FR',
+        alias: ['fr'],
+        antdLocale: frFR,
+        messages: frFRMessages,
     },
 ]
 
