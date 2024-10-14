b9ecfe5badeda7c0b04941c0f7675a3da640daf0
Kasra Bigdeli <kasra85@gmail.com>
2024-10-13 21:44:20 -0700

    Added extra for theme

--- src/styles/theme/CapRoverTheme.ts
+++ src/styles/theme/CapRoverTheme.ts
@@ -1,5 +1,10 @@
+interface CapRoverExtraTheme {
+    siderTheme?: string
+}
+
 export default interface CapRoverTheme {
     content: string
     name: string
+    extraTheme?: CapRoverExtraTheme
     builtIn?: boolean
 }
