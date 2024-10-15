2165f0a78df4b0d059a24f0376b63fc879690075
Kasra Bigdeli <kasra85@gmail.com>
2024-10-14 23:50:16 -0700

    Added simple css

--- src/containers/apps/Apps.tsx
+++ src/containers/apps/Apps.tsx
@@ -73,7 +73,7 @@ export default class Apps extends ApiComponent<
         }
 
         return (
-            <div>
+            <div className="slow-fadein-fast">
                 <div
                     style={{
                         display: 'flex',
